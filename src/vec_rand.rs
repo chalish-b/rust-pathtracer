93c91a46d3944763683164dc34467c3b43a221ed
chalish <git@chalish.dev>
2026-02-20 16:27:49 +0300

    Faster random_unit_vec implementation
    Improves the render times by ~20% on non-release

--- src/vec_rand.rs
+++ src/vec_rand.rs
@@ -1,7 +1,10 @@
+use std::f32;
+
 use glam::Vec3;
 use rand::RngExt;
 
-pub fn random_vec(min: f32, max: f32) -> Vec3 {
+// Not needed anymore since it was only required by the old random_unit_vec algo
+pub fn _random_vec(min: f32, max: f32) -> Vec3 {
     let mut rng = rand::rng();
     Vec3 {
         x: rng.random_range(min..=max),
@@ -11,14 +14,21 @@ pub fn random_vec(min: f32, max: f32) -> Vec3 {
 }
 
 pub fn random_unit_vec() -> Vec3 {
-    // The naive approach is to sample until we land inside the unit sphere, so that it's not biased
-    // TODO: Replace this with a one shot method
-    loop {
-        let vec = random_vec(-1.0, 1.0);
-        let len_squared = vec.length_squared();
-        if 0.001 < len_squared && len_squared <= 1.0 {
-            return vec.normalize();
-        }
+    let mut rng = rand::rng();
+
+    // Sampling uniformly on a sphere:
+    // - Pick a z coordinate uniformly (not the polar angle) from [-1, 1]
+    // - Pick theta (azimuthal angle) uniformly from [0, 2pi]
+    let z = rng.random_range(-1.0..=1.0f32);
+    let theta = rng.random_range(0.0..(2.0 * f32::consts::PI));
+
+    // This is the radius on the xy plane
+    let r = (1.0 - z * z).sqrt();
+
+    Vec3 {
+        x: r * theta.cos(),
+        y: r * theta.sin(),
+        z,
     }
 }
 
