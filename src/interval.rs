34484beb0ab9b0cb437cf4cc8c0daa53f52c4d61
chalish <git@chalish.dev>
2026-07-07 05:56:11 +0300

    Add overlaps method

--- src/interval.rs
+++ src/interval.rs
@@ -5,4 +5,8 @@ impl Interval {
     pub fn contains(self, t: f32) -> bool {
         self.0 <= t && t <= self.1
     }
+
+    pub fn overlaps(self, other: Interval) -> bool {
+        self.0 <= other.1 && other.0 <= self.1
+    }
 }
